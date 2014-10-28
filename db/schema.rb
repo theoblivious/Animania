910df6efdb063d4ad22059798bf7100270317082
theoblivious <hashimoto.kenn@gmail.com>
2014-10-28 12:29:32 -0400

    pushing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027215502) do
+ActiveRecord::Schema.define(version: 20141028152924) do
+
+  create_table "anime_libraries", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "anime_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "animes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "questions", force: true do |t|
+    t.string   "english"
+    t.string   "japanese"
+    t.integer  "anime_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
