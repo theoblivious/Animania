28195d2c24993a46d3d7ebe6b4441213f6b41182
theoblivious <hashimoto.kenn@gmail.com>
2014-11-30 20:53:42 -0500

    more changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,32 +17,3 @@ end
 
 
 
-# animes = Anime.create([{name: 'FLCL Episode 1'}, {name: 'FLCL Episode 2'}])
-# Questions_anime1 =Question.create([
-#                                     {japanese:"konnichiwa", english: "hello", anime_id: 1},
-#                                     {japanese:"Korosu", english: "kill", anime_id: 1}
-
-
-# ])
-
-# Questions_anime_2 =Question.create([
-#                                      {japanese:"Chimatsuri", english: "blood bath", anime_id: 2},
-#                                      {japanese:"Genki Desuka?", english: "Are you well?", anime_id: 2}
-
-
-
-# ])
-
-
-
-
-
-
-# t.string   "email",                  default: "", null: false
-#     t.string   "encrypted_password",     default: "", null: false
-#     t.string   "reset_password_token"
-#     t.datetime "reset_password_sent_at"
-#     t.datetime "remember_created_at"
-#     t.integer  "sign_in_count",          default: 0,  null: false
-#     t.datetime "current_sign_in_at"
-#     t.datetime "last_sign_in_at"
